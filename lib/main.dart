4ccd935efb5283bf05c51e9f74dcd17385ef8f2e
Wkerlyson <wkerlyson.batista@gmail.com>
2019-10-22 18:38:19 -0300

    adding ScrollView and RaisedButton

--- lib/main.dart
+++ lib/main.dart
@@ -12,37 +12,58 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI Calculator'),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.refresh),
-            onPressed: () {},
-          )
-        ],
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Icon(Icons.person_outline, size: 120, color: Colors.green),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: 'Weight (Kg)',
-                labelStyle: TextStyle(color: Colors.green)),
-            style: TextStyle(color: Colors.green, fontSize: 25),
+        appBar: AppBar(
+          title: Text('BMI Calculator'),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {},
+            )
+          ],
+        ),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(Icons.person_outline, size: 120, color: Colors.green),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: 'Weight (kg)',
+                    labelStyle: TextStyle(color: Colors.green)),
+                style: TextStyle(color: Colors.green, fontSize: 15),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: 'Height (cm)',
+                    labelStyle: TextStyle(color: Colors.green)),
+                style: TextStyle(color: Colors.green, fontSize: 15),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 20, top: 40),
+                child: Container(
+                  height: 50,
+                  child: RaisedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Calculate',
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    ),
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+              Text(
+                'Nota',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 15),
+              )
+            ],
           ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: 'Height (cm)',
-                labelStyle: TextStyle(color: Colors.green)),
-            style: TextStyle(color: Colors.green, fontSize: 25),
-          )
-        ],
-      ),
-    );
+        ));
   }
 }
