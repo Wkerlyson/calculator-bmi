2f639baec356a91799b1e8732b039490f6f81e08
Wkerlyson <wkerlyson.batista@gmail.com>
2019-10-22 22:26:14 -0300

    Forms and validations

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # App for Body Mass Index calculation
 
 App #2 of course [Criação de Apps Android e iOS com Flutter - Crie 16 Apps](https://www.udemy.com/course/curso-completo-flutter-app-android-ios/)
+
+<img src="./assets/images/screenshot_01.jpg" width="220" height="400"> <img src="./assets/images/screenshot_02.jpg" width="220" height="400">
--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,16 @@ class _HomeState extends State<Home> {
   TextEditingController weightController = TextEditingController();
   TextEditingController heightController = TextEditingController();
 
+  GlobalKey<FormState> _formKey = GlobalKey();
+
   String _infoText = 'Enter your data';
 
   void _resetFields() {
     weightController.text = '';
     heightController.text = '';
-    _infoText = 'Enter your data';
+    setState(() {
+      _infoText = 'Enter your data';
+    });
   }
 
   void _calculate() {
@@ -25,18 +29,17 @@ class _HomeState extends State<Home> {
       double weight = double.parse(weightController.text);
       double height = double.parse(heightController.text) / 100;
       double bmi = weight / (height * height);
-      print(bmi);
 
       if (bmi < 18.6) {
-        _infoText = "Underweight (${bmi.toStringAsPrecision(4)})";
+        _infoText = "Underweight - BMI: ${bmi.toStringAsPrecision(4)}";
       } else if (bmi >= 18.6 && bmi < 24.9) {
-        _infoText = "Ideal weight (${bmi.toStringAsPrecision(4)})";
+        _infoText = "Ideal weight - BMI: ${bmi.toStringAsPrecision(4)}";
       } else if (bmi >= 24.9 && bmi < 29.9) {
-        _infoText = "Slightly overweight (${bmi.toStringAsPrecision(4)})";
+        _infoText = "Slightly overweight - BMI: ${bmi.toStringAsPrecision(4)}";
       } else if (bmi >= 29.9 && bmi < 34.9) {
-        _infoText = "Obesity Grade I (${bmi.toStringAsPrecision(4)})";
+        _infoText = "Obesity Grade I - BMI: ${bmi.toStringAsPrecision(4)}";
       } else if (bmi >= 34.9 && bmi < 39.9) {
-        _infoText = "obesity Grade II (${bmi.toStringAsPrecision(4)})";
+        _infoText = "obesity Grade II - BMI: (${bmi.toStringAsPrecision(4)}";
       } else if (bmi >= 40) {
         _infoText = "Obesity Grade III (${bmi.toStringAsPrecision(4)})";
       }
@@ -58,48 +61,58 @@ class _HomeState extends State<Home> {
           ],
         ),
         body: SingleChildScrollView(
-          padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Icon(Icons.person_outline, size: 120, color: Colors.green),
-              TextField(
-                keyboardType: TextInputType.number,
-                controller: weightController,
-                decoration: InputDecoration(
-                    labelText: 'Weight (kg)',
-                    labelStyle: TextStyle(color: Colors.green)),
-                style: TextStyle(color: Colors.green, fontSize: 15),
-              ),
-              TextField(
-                keyboardType: TextInputType.number,
-                controller: heightController,
-                decoration: InputDecoration(
-                    labelText: 'Height (cm)',
-                    labelStyle: TextStyle(color: Colors.green)),
-                style: TextStyle(color: Colors.green, fontSize: 15),
-              ),
-              Padding(
-                padding: EdgeInsets.only(bottom: 20, top: 40),
-                child: Container(
-                  height: 50,
-                  child: RaisedButton(
-                    onPressed: _calculate,
-                    child: Text(
-                      'Calculate',
-                      style: TextStyle(color: Colors.white, fontSize: 18),
-                    ),
-                    color: Colors.green,
+            padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Icon(Icons.person_outline, size: 120, color: Colors.green),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    controller: weightController,
+                    decoration: InputDecoration(
+                        labelText: 'Weight (kg)',
+                        labelStyle: TextStyle(color: Colors.green)),
+                    style: TextStyle(color: Colors.green, fontSize: 15),
+                    validator: (value) {
+                      if (value.isEmpty) return 'Enter your weight';
+                    },
                   ),
-                ),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    controller: heightController,
+                    decoration: InputDecoration(
+                        labelText: 'Height (cm)',
+                        labelStyle: TextStyle(color: Colors.green)),
+                    style: TextStyle(color: Colors.green, fontSize: 15),
+                    validator: (value) {
+                      if (value.isEmpty) return 'Enter your height';
+                    },
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 20, top: 40),
+                    child: Container(
+                      height: 50,
+                      child: RaisedButton(
+                        onPressed: () {
+                          if (_formKey.currentState.validate()) _calculate();
+                        },
+                        child: Text(
+                          'Calculate',
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        ),
+                        color: Colors.green,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    _infoText,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.green, fontSize: 15),
+                  )
+                ],
               ),
-              Text(
-                _infoText,
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.green, fontSize: 15),
-              )
-            ],
-          ),
-        ));
+            )));
   }
 }
